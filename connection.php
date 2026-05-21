a695b9f0dca70d71267d29b44f1bb31428d1692f
Chaz <thecockroachjoey@gmail.com>
2026-05-21 14:29:06 +0800

    Config: Set master timezone to Asia/Manila for accurate timestamp logging

--- connection.php
+++ connection.php
@@ -1,13 +1,20 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "webadmin";
+$password = "Admin123!";
 $dbname = "ims480";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
+// 1. Force PHP web server to use Philippine Time
+date_default_timezone_set('Asia/Manila');
+
+// 2. Force MySQL database to stamp new records in Philippine Time (UTC+8)
+$conn->query("SET time_zone = '+08:00'");
 ?>
